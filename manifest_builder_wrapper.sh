b759d40cf377c05a92195191d68147f26cf6421f
jgpawletko <jgpawletko@gmail.com>
2015-07-24 14:00:28 -0400

    change [ ] to [[ ]]

--- manifest_builder_wrapper.sh
+++ manifest_builder_wrapper.sh
@@ -6,22 +6,22 @@
 #
 echo "Reading config...." >&2
          
-if [ ! -f builder_config.cfg ]; then
+if [[ ! -f builder_config.cfg ]]; then
  echo "Config file builder_config.cfg doesn't exist. Please provide one" 
  exit 1 
 fi
              
 source builder_config.cfg
 
-if [ ! -d $SOURCE_DIR ]; then
+if [[ ! -d $SOURCE_DIR ]]; then
  echo "SOURCE_DIR:$SOURCE_DIR doesn't exist."  
  exit 1
 fi  
 
 declare -a VIDEOS
 
-if [ $# -gt 0 ]; then
-        if [ ! -f $1 ]; then
+if [[ $# -gt 0 ]]; then
+        if [[ ! -f $1 ]]; then
            echo "file $1 doesn't exist. Please check your path "
            exit 1
         fi
@@ -33,9 +33,9 @@ fi
 for VIDEO in $VIDEOS
 do
 	echo Processing $VIDEO
-	./video_manifest_builder.sh $VIDEO
+	./video_manifest_builder.sh $VIDEO 
 	RETVAL=$?
-	if [ $RETVAL -eq 0 ]; then
+	if [[ $RETVAL -eq 0 ]]; then
 		STATUS=PASS
 	else
 		STATUS=FAIL
