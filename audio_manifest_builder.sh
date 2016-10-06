4bfcfa08b17c27b1f8d80254c45bbcfb20fe0829
Ekaterina Penchekhonova <ekate@nyu.edu>
2016-10-06 16:27:14 -0400

    Fixed file name and added test content

--- audio_manifest_builder.sh
+++ audio_manifest_builder.sh
@@ -13,7 +13,7 @@ print_error () {
 
 print_usage () {
     echo "usage: $0 <audio id> <audio dir> <partner code> <collection code>"
-    echo " e.g.: $0 326_1019_00002 /content/prod/rstar/content/fales/mss326/wip/se/326_0071_00001/aux fales mss326"
+    echo " e.g.: $0 326_1019 /content/prod/rstar/content/fales/mss326/wip/se/ fales mss326"
 }
 
 #read and check parameters
@@ -24,7 +24,12 @@ get_param () {
 
 get_file_name () {
     local file_name=$(basename "$1")
-    echo "$file_name"
+    echo "${file_name}"
+}
+
+get_audio_name () {
+   local audio_name=$(basename "$1")
+   echo "${audio_name%_s.m4a}"
 }
 
 delete_old_manifest () {
@@ -33,31 +38,23 @@ delete_old_manifest () {
     fi
 }
 
-generate_m3u8_manifest () {
+generate_m3u8_manifest ()  {
     echo "#EXTM3U">>${M3U8_MANIFEST}
-    echo "#AUDIO_ID:${AUDIO_ID}">>${M3U8_MANIFEST}
+    echo "#AUDIO_ID:$2">>${M3U8_MANIFEST}
     echo ''>>${M3U8_MANIFEST}
-    for f in  ${AUDIO_DIR}/${AUDIO_ID}_s.m4a
-    do
-	fr=$(get_file_name "$f")
-	echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=100000">>${M3U8_MANIFEST}
-	echo "">>${M3U8_MANIFEST}
-	echo "${BASE_URL_HLS}/$fr.m3u8">>${M3U8_MANIFEST}
-	echo "">>${M3U8_MANIFEST}
-    done
+    echo "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=100000">>${M3U8_MANIFEST}
+    echo "">>${M3U8_MANIFEST}
+    echo "${BASE_URL_HLS}/$1.m3u8">>${M3U8_MANIFEST}
+    echo "">>${M3U8_MANIFEST}
 }
 
 generate_f4m_manifest () {
     echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>">>${F4M_MANIFEST}
     echo "<manifest xmlns=\"http://ns.adobe.com/f4m/1.0\">">>${F4M_MANIFEST}
-    echo "<id>${AUDIO_ID}</id>">>${F4M_MANIFEST}
+    echo "<id>$2</id>">>${F4M_MANIFEST}
     echo "<baseURL>${BASE_URL_HDS}</baseURL>">>${F4M_MANIFEST}
     echo "<mimeType>audio/mp4</mimeType>">>${F4M_MANIFEST}
-    for f in  ${AUDIO_DIR}/${AUDIO_ID}_s.m4a
-    do
-	fr=$(get_file_name "$f")
-	echo "<media url=\"mp4:${fr%.mp4}\" />">>${F4M_MANIFEST}
-    done
+    echo "<media url=\"mp4:$1\" />">>${F4M_MANIFEST}
     echo "</manifest>">>${F4M_MANIFEST}
 }
 
@@ -78,18 +75,24 @@ if [[ ! -d ${AUDIO_DIR} ]]; then
     echo "AUDIO_DIR:${AUDIO_DIR} doesn\'t exist." 
     exit 1
 fi
+    echo "AUDIO_DIR:${AUDIO_DIR}" 
 
 #define variables 
-M3U8_MANIFEST=${AUDIO_DIR}/"${AUDIO_ID}"_"$M3U8"
-F4M_MANIFEST=${AUDIO_DIR}/"${AUDIO_ID}"_"$F4M"
 BASE_URL_HDS=rtmp://${AUDIO_SERVER_NAME}/${APP_NAME}
 BASE_URL_HLS=http://${AUDIO_SERVER_NAME}/hls-vod/audio-only-aac/${APP_NAME_HLS}
 
-#generate hls manifest file- extention u8m3
-delete_old_manifest ${M3U8_MANIFEST}
-generate_m3u8_manifest
-
-#generates hds manifest file - extention f4m 
-delete_old_manifest ${F4M_MANIFEST}
-generate_f4m_manifest
-
+#iterate over audio files in the directory
+for f in  ${AUDIO_DIR}/*_s.m4a
+do
+    fr=$(get_file_name "$f")
+    a_name=$(get_audio_name "$fr") 
+    #define variables
+    M3U8_MANIFEST=${AUDIO_DIR}/"$a_name"_"$M3U8"
+    F4M_MANIFEST=${AUDIO_DIR}/"$a_name"_"$F4M"
+    #generate hls manifest file- extention u8m3
+    delete_old_manifest ${M3U8_MANIFEST}
+    generate_m3u8_manifest "$fr" "$a_name"
+    #generates hds manifest file - extention f4m 
+    delete_old_manifest ${F4M_MANIFEST}
+    generate_f4m_manifest "$fr" "$a_name"
+done
